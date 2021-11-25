ce2030547a572508793a039fc116c6da434c30e3
Quentin-Filleule <65295378+Quentin-Filleule@users.noreply.github.com>
2021-11-25 09:30:47 +0100

    add fct magasin

--- Magasin.cpp
+++ Magasin.cpp
@@ -3,3 +3,33 @@
 Magasin::Magasin(std::string name) : _namemagasin(name) {
 
 }
+
+
+void Magasin::addProduit(Produit _produit){
+
+_produits.push_back(_produit);
+}
+
+void Magasin::afficheMagasin(std::vector<Produit> _produits){
+
+    for (int i=0; i < _produits.size(); i++)
+    {
+        std::cout << _produits.at(i) << std::endl;
+    }
+}
+
+
+std::string Magasin::findProduit(std::vector<Produit> _produits) const{
+
+        std::string _recherche;
+        sdt::cin>> _recherche;
+
+        for (int i=0; i < _produits.size(); i++)
+    {
+        if (_produit == _recherche)
+        {
+            std::cout << _produits.at(i) << std::endl
+        }
+    }
+
+}
--- Magasin.h
+++ Magasin.h
@@ -1,17 +1,25 @@
 #pragma once
 #include "string"
+#include "Produit.h"
+#include <vector>
+#include <iostream>
 
 class Magasin
 {
 private :
 
 	std::string _namemagasin;
+	std::vector<Produit> _produits;
+
+
+
 
 public :
 
 	Magasin(std::string _namemagasin);
-
-
+	void addProduit(Produit _produit);
+	void afficheMagasin(std::vector<Produit> _produits);
+	std::string findProduit(std::vector<Produit> _produits) const;
 
 };
 
