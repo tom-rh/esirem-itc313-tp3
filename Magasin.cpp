e6a4ea36dd1ffacd380f51e8ddf505c2887094e0
tom-rh <64775524+tom-rh@users.noreply.github.com>
2021-11-25 19:25:08 +0100

    Update Magasin.cpp

--- Magasin.cpp
+++ Magasin.cpp
@@ -6,12 +6,10 @@ Magasin::Magasin(std::string name) : _namemagasin(name) {
 
 
 void Magasin::addProduit(Produit _produit){
-
-_produits.push_back(_produit);
+	_produits.push_back(_produit);
 }
 
 void Magasin::afficheMagasin(std::vector<Produit> _produits) {
-
     for (int i=0; i < _produits.size(); i++)
     {
         std::cout << _produits.at(i) << std::endl;
@@ -20,30 +18,19 @@ void Magasin::afficheMagasin(std::vector<Produit> _produits) {
 
 
 void Magasin::findProduit(std::vector<Produit> _produits,std::string _recherche) const {
-
-        for (int i=0; i < _produits.size(); i++) {
-           
-            if ( _produits.at(i).getNameProduit() == _recherche)
-            {
-                std::cout << _produits.at(i) << std::endl;
-               
-            }
-
-        }
+	for (int i=0; i < _produits.size(); i++) {
+		if ( _produits.at(i).getNameProduit() == _recherche)
+		{
+			std::cout << _produits.at(i) << std::endl;
+		}
+	}
 }
 
 void Magasin::changeQtite(std::vector<Produit> _produits, int _qtite,std::string _recherche){
-
-double max;
-
    for (int i=0; i < _produits.size(); i++) {
-           
-            if ( _produits.at(i).getNameProduit() == _recherche)
-            {
-               max=_produits.at(i).getQtite();
-               _produits.at(i).setQtite(max+_qtite);
-
-            }
-
-        }
+		if ( _produits.at(i).getNameProduit() == _recherche)
+		{
+			_produits.at(i).setQuantite(_produits.at(i).getQuantite()+_qtite);
+		}
+    }
 }
