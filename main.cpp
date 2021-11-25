cdbc18692dd8a6f7beb6b23a12dc84028f75f7ae
tom-rh <64775524+tom-rh@users.noreply.github.com>
2021-11-25 19:46:04 +0100

    test class in main

--- main.cpp
+++ main.cpp
@@ -1,12 +1,25 @@
 #include "Magasin.h"
+#include "Client.h"
 
 int main()
 {
 	Magasin magasin("EasyStock");
 
+	Produit p1(1, "PS5", "Console de jeux", 5, 600);
 
+	std::cout << p1 << std::endl;
 
+	Client c1(1, "Tom", "Roth");
+
+	c1.ajouterProduitPanier(p1);
+
+	std::cout << c1;
+
+	c1.modifierQuantiteProduitPanier(p1, 2);
+
+	std::cout << "Ajout de 2 produits dans le panier" << std::endl;
+
+	std::cout << c1;
 
 	return 0;
-
 }
