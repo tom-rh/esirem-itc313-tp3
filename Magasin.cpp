9559c7bd4b2778809f15a257503fff75b650b9d2
tom-rh <64775524+tom-rh@users.noreply.github.com>
2021-11-25 19:51:53 +0100

    Qtite -> Quantite

--- Magasin.cpp
+++ Magasin.cpp
@@ -4,7 +4,6 @@ Magasin::Magasin(std::string name) : _namemagasin(name) {
 
 }
 
-
 void Magasin::addProduit(Produit _produit){
 	_produits.push_back(_produit);
 }
@@ -16,8 +15,7 @@ void Magasin::afficheMagasin(std::vector<Produit> _produits) {
     }
 }
 
-
-void Magasin::findProduit(std::vector<Produit> _produits,std::string _recherche) const {
+void Magasin::trouverProduit(std::vector<Produit> _produits,std::string _recherche) const {
 	for (int i=0; i < _produits.size(); i++) {
 		if ( _produits.at(i).getNameProduit() == _recherche)
 		{
@@ -26,11 +24,11 @@ void Magasin::findProduit(std::vector<Produit> _produits,std::string _recherche)
 	}
 }
 
-void Magasin::changeQtite(std::vector<Produit> _produits, int _qtite,std::string _recherche){
+void Magasin::changerQuantite(std::vector<Produit> _produits, int _quantite,std::string _recherche){
    for (int i=0; i < _produits.size(); i++) {
 		if ( _produits.at(i).getNameProduit() == _recherche)
 		{
-			_produits.at(i).setQuantite(_produits.at(i).getQuantite()+_qtite);
+			_produits.at(i).setQuantite(_produits.at(i).getQuantite()+_quantite);
 		}
     }
 }
--- Magasin.h
+++ Magasin.h
@@ -7,21 +7,13 @@
 class Magasin
 {
 private :
-
 	std::string _namemagasin;
 	std::vector<Produit> _produits;
-
-
-
-
 public :
-
 	Magasin(std::string _namemagasin);
 	void addProduit(Produit _produit);
 	void afficheMagasin(std::vector<Produit> _produits);
-	void findProduit(std::vector<Produit> _produits,std::string _recherche) const;
-	void changeQtite(std::vector<Produit> _produits, int _qtite,std::string _recherche);
-
-
+	void trouverProduit(std::vector<Produit> _produits, std::string _recherche) const;
+	void changerQuantite(std::vector<Produit> _produits, int _quantite, std::string _recherche);
 };
 
