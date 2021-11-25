1a2f7785ae4685a0509d4d24cc61462c32b3aa2e
tom-rh <64775524+tom-rh@users.noreply.github.com>
2021-11-25 19:45:13 +0100

    Update surcharge - Class client

--- Client.cpp
+++ Client.cpp
@@ -22,6 +22,7 @@ std::vector<Produit> Client::getPanier() const {
 }
 
 void Client::ajouterProduitPanier(Produit produit) {
+	//Supprimer 1 produit du magasin
     _panier.push_back(produit);
 }
 
@@ -32,6 +33,7 @@ void Client::viderPanier() {
 void Client::modifierQuantiteProduitPanier(Produit produit, int quantite) {
 	for (std::vector<Produit>::iterator it = _panier.begin() ; it != _panier.end(); ++it) {
 		if (produit.getId() == (*it).getId()) {
+				//Supprimer quantite produit du magasin
 				(*it).setQuantite(quantite);
 			break;
 		}
@@ -46,12 +48,14 @@ std::ostream& operator<<(std::ostream& os, const Client& client)
 {
     os << "Fiche client - Numero " << client.getId() << std::endl;
 	os << "Prenom et nom : " << client.getPrenom() << " " << client.getNom() << std::endl;
-    os << "Panier :" << std::endl;
+    os << "Panier (" << client.getPanier().size() << ") :" << std::endl;
     if (client.getPanier().empty())
         os << "Vide" << std::endl;
     else {
-        for (std::vector<Produit>::iterator it = client.getPanier().begin() ; it != client.getPanier().end(); ++it) {
-			os << *it << std::endl;
+		for (auto i = 0; i != client.getPanier().size(); i++) {
+			os << std::endl;
+			os << "Produit " << i+1 << std::endl;
+			os << client.getPanier().at(i) << std::endl;
 		}
     }
 	return os;
