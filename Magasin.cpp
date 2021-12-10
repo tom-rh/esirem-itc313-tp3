6106c7ba4b06bc9a1ce516224cbf2a6492aa9a5f
Tom <64775524+tom-rh@users.noreply.github.com>
2021-12-10 11:42:47 +0100

    Add methods commands in magasin class

--- Magasin.cpp
+++ Magasin.cpp
@@ -37,52 +37,61 @@ void Magasin::addClient(Client _client){
 	_clients.push_back(_client);
 }
 
-
-void Magasin::afficheClient(std::vector<Client> _clients) {
+void Magasin::afficheClient() {
     for (int i=0; i < _clients.size(); i++)
     {
         std::cout << _clients.at(i) << std::endl;
     }
 }
 
-
-void Magasin::trouverClient(std::vector<Client> _clients,std::string _recherche) const {
+void Magasin::trouverClient(std::string _recherche) const {
 
 	int rch;
 
 	for (int i=0; i < _clients.size(); i++) {
 		if ( _clients.at(i).getNom() == _recherche)
-		{
 			std::cout << _produits.at(i) << std::endl;
-		}
-		
 		else if (_clients.at(i).getId() == (rch=std::stoi(_recherche)))
-		{
 			std::cout << _produits.at(i) << std::endl;
-		}
-
-		else{
+		else
 			std::cout << "aucun client correspondant" << std::endl;
-		}
+	}
 }
-}
 
-
-void Magasin::addProduitToPanier(Produit _produit,Client _client){
-
+void Magasin::addProduitToPanier(Produit _produit,Client _client) {
 	_client.ajouterProduitPanier(_produit);
 }
 
 
-void Magasin::suprProduitToPanier(Produit _produit,Client _client){
-
+void Magasin::suprProduitToPanier(Produit _produit,Client _client) {
 	_client.supprimerProduitPanier(_produit);
 }
 
 
 void Magasin::qtitProduitToPanier(Produit _produit,Client _client,int qtit){
-
 	_client.modifierQuantiteProduitPanier(_produit, qtit);
 }
 
+bool Magasin::validerCommande(Commande _commande) {
+	// TODO
+	_commande.getClient().viderPanier();
+}
 
+bool Magasin::setStatutCommande(Commande _commande, int _statut) {
+	for (auto i = 0; i != _commandes.size(); i++) {
+		// TODO
+    }
+}
+
+void Magasin::afficheCommandes() const {
+	for (auto i = 0; i != _commandes.size(); i++) {
+        std::cout << _commandes.at(i) << std::endl;
+    }
+}
+
+void Magasin::afficheCommandesClient(Client _client) const {
+	for (auto i = 0; i != _commandes.size(); i++) {
+		if (_client.getId() == _commandes.at(i).getClient().getId())
+        	std::cout << _commandes.at(i) << std::endl;
+    }
+}
