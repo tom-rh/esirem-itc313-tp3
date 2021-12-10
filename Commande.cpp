ec2d1ad05645e053947de773a898157837bf599c
Tom <64775524+tom-rh@users.noreply.github.com>
2021-12-10 09:03:50 +0100

    add class commande

---
+++ Commande.cpp
@@ -0,0 +1,27 @@
+#include "Commande.h"
+#include <iostream>
+
+Client Commande::getClient() const {
+    return _client;
+}
+
+std::vector<Produit> Commande::getProduitsAchetes() const{
+    return _produitsAchetes;
+}
+
+int Commande::getStatut() const{
+    return _statut;
+}
+
+std::ostream& operator<<(std::ostream& os, const Commande& commande)
+{
+    os << "Commande :" << std::endl;
+	os << "Client : " << commande.getClient() << std::endl;
+    os << "Produits achetés : " << std::endl;
+    for (auto i = 0; i != commande.getProduitsAchetes().size(); i++) {
+        os << std::endl;
+        os << "Produit " << i+1 << std::endl;
+        os << commande.getProduitsAchetes().at(i) << std::endl;
+    }
+	return os;
+}
---
+++ Commande.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Client.h"
+#include "Produit.h"
+
+class Commande
+{
+private:
+    Client _client;
+    std::vector<Produit> _produitsAchetes;
+    int _statut;
+public:
+    Client getClient() const;
+    std::vector<Produit> getProduitsAchetes() const;
+    int getStatut() const;
+};
+
+std::ostream& operator<<(std::ostream& os, const Client& client);
