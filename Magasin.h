6ba9926fc701be13c1c8f770518a25bccf2b2db9
Tom <64775524+tom-rh@users.noreply.github.com>
2021-12-10 11:42:50 +0100

    Update Magasin.h

--- Magasin.h
+++ Magasin.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include <iostream>
 #include "Client.h"
+#include "Commande.h"
 
 class Magasin
 {
@@ -11,6 +12,7 @@ private :
 	std::string _namemagasin;
 	std::vector<Produit> _produits;
 	std::vector<Client> _clients;
+	std::vector<Commande> _commandes;
 
 public :
 	Magasin(std::string _namemagasin);
@@ -19,11 +21,14 @@ public :
 	void trouverProduit(std::string _recherche) const;
 	void changerQuantite(int _quantite, std::string _recherche);
 	void addClient(Client _client);
-	void afficheClient(std::vector<Client> _clients);
-	void trouverClient(std::vector<Client> _clients, std::string _recherche) const;
+	void afficheClient();
+	void trouverClient(std::string _recherche) const;
 	void addProduitToPanier(Produit _produit,Client _client);
 	void suprProduitToPanier(Produit _produit,Client _client);
 	void qtitProduitToPanier(Produit _produit,Client _client, int qtit);
-
+	bool validerCommande(Commande _commande);
+	bool setStatutCommande(Commande _commande, int _statut);
+	void afficheCommandes() const;
+	void afficheCommandesClient(Client _client) const;
 };
 
